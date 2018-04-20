4da25d193e266bd8aeb0d4e65e238c19e4a27b09
RebuildYan <hyyanis66@gmail.com>
2018-04-20 18:37:41 +0800

    增加小数类型

--- strategy.js
+++ strategy.js
@@ -20,5 +20,19 @@ export default {
                 max: 105
             }
         ]
+    },
+    decimal: {
+        legalReg: [/[0-9.]{1,}$/],
+        legalKeyCode: [8, 32, 37, 38, 39, 40, 46, 190],
+        legalKeyCodeRange: [
+            {
+                min: 48,
+                max: 57
+            },
+            {
+                min: 96,
+                max: 105
+            }
+        ]
     }
 };
